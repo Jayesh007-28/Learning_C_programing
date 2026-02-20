82f8658526357be8ea91a06dcbef1f78854fcfa3
Jayesh <jayeshajay2003@gmail.com>
2026-02-20 22:20:17 +0530

    Sort array in ascending and descending order

---
+++ array_sort.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main(){
+	int arr[5] = {20,90,60,10,30};
+	int i,j,temp;
+	//Asending order
+	
+	for(i=0;i<5-1;i++){
+		for(j=i+1;j<5;j++){
+			if(arr[i]>arr[j]){
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+	printf("Sorded array in asending order : \n");
+	for(i=0;i<5;i++){
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+	//descending order
+	for(i=0;i<5-1;i++){
+		for(j=i+1;j<5;j++){
+			if(arr[i]<arr[j]){
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+	
+	printf("Sorded array in descending order : \n");
+	for(i=0;i<5;i++){
+		printf("%d ",arr[i]);
+	}
+	
+	return 0;
+}
