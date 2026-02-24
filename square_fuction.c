3d634cd4dbc63277534440edbb9166eb45d5bd11
Jayesh <jayeshajay2003@gmail.com>
2026-02-24 18:00:18 +0530

    //write a function to find a square of num with arg no return

---
+++ square_fuction.c
@@ -0,0 +1,14 @@
+//write a function to find a square of num with arg no return
+
+#include<stdio.h>
+
+void squareNum(int num){
+	printf("Square of num = %d",num*num);
+}
+
+int main(){
+	
+	squareNum(10);
+	
+	return 0;
+}
